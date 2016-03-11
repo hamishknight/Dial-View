16e6f3126b1f46640928da705be8484524d1e221
Hamish Knight <hk@southerncare.co.uk>
2016-03-11 17:21:55 +0000

    Initial Commit

---
+++ Arc GraduationTests/Arc_GraduationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Arc_GraduationTests.swift
+//  Arc GraduationTests
+//
+//  Created by Hamish Knight on 11/03/2016.
+//  Copyright © 2016 Redonkulous Apps. All rights reserved.
+//
+
+import XCTest
+@testable import Arc_Graduation
+
+class Arc_GraduationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
